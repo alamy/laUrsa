161ffd708b70dc38e682377256fadd1ab01e0247
a.verissimo <alamy.neto@acpgroup.com.br>
2023-05-10 17:02:16 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-import * as Logo from '../src/lib/components/Icon/Logo';
+import * as Logo from './lib/components/Icon/Logo';
 // Icon System
-import * as Eye from '../src/lib/components/Icon/System/Eye';
-import * as EyeNot from '../src/lib/components/Icon/System/EyeNot';
-import * as Mic from '../src/lib/components/Icon/System/Mic';
-import * as Search from '../src/lib/components/Icon/System/Search';
-import * as X from '../src/lib/components/Icon/System/X';
+import * as Eye from './lib/components/Icon/System/Eye';
+import * as EyeNot from './lib/components/Icon/System/EyeNot';
+import * as Mic from './lib/components/Icon/System/Mic';
+import * as Search from './lib/components/Icon/System/Search';
+import * as X from './lib/components/Icon/System/X';
 
 // Icon Trilha
-import * as Aprender from '../src/lib/components/Icon/Trilha/Aprender';
-import * as Defeciencia from '../src/lib/components/Icon/Trilha/Defeciencia';
-import * as Curtir from '../src/lib/components/Icon/Trilha/Curtir';
-import * as Empresario from '../src/lib/components/Icon/Trilha/Empresario';
-import * as Idoso from '../src/lib/components/Icon/Trilha/Idoso';
-import * as Lgbt from '../src/lib/components/Icon/Trilha/Lgbt';
-import * as Mulher from '../src/lib/components/Icon/Trilha/Mulher';
-import * as Servidor from '../src/lib/components/Icon/Trilha/Servidor';
-import * as Turista from '../src/lib/components/Icon/Trilha/Turista';
+import * as Aprender from './lib/components/Icon/Trilha/Aprender';
+import * as Defeciencia from './lib/components/Icon/Trilha/Defeciencia';
+import * as Curtir from './lib/components/Icon/Trilha/Curtir';
+import * as Empresario from './lib/components/Icon/Trilha/Empresario';
+import * as Idoso from './lib/components/Icon/Trilha/Idoso';
+import * as Lgbt from './lib/components/Icon/Trilha/Lgbt';
+import * as Mulher from './lib/components/Icon/Trilha/Mulher';
+import * as Servidor from './lib/components/Icon/Trilha/Servidor';
+import * as Turista from './lib/components/Icon/Trilha/Turista';
 
 //Atomic
-import * as Avatar from '../src/lib/components/Atomic/Avatar/Avatar/';
-import * as Button from '../src/lib/components/Atomic/Button/Button';
-import * as Checkbox from '../src/lib/components/Atomic/Checkbox/Checkbox';
-import * as Dropdown from '../src/lib/components/Atomic/Dropdown/Dropdown';
-import * as Input from '../src/lib/components/Atomic/Input/Input';
-import * as Radio from '../src/lib/components/Atomic/Radio/Radio';
+import * as Avatar from './lib/components/Atomic/Avatar/Avatar/';
+import * as Button from './lib/components/Atomic/Button/Button';
+import * as Checkbox from './lib/components/Atomic/Checkbox/Checkbox';
+import * as Dropdown from './lib/components/Atomic/Dropdown/Dropdown';
+import * as Input from './lib/components/Atomic/Input/Input';
+import * as Radio from './lib/components/Atomic/Radio/Radio';
 
 // Molecules
-import * as Table from '../src/lib/components/Molecules/Table/Table';
+import * as Table from './lib/components/Molecules/Table/Table';
 
 //Template
-import * as Header from '../src/lib/components/Templates/Header';
+import * as Header from './lib/components/Templates/Header';
 export { Logo,
 // Icon System
 Eye, EyeNot, Mic, Search, X,
@@ -41,31 +41,31 @@ Avatar, Button, Checkbox, Dropdown, Input, Radio,
 Table,
 //Template
 Header };
-export * from '../src/lib/components/Icon/Logo';
+export * from './lib/components/Icon/Logo';
 // Icon System
-export * from '../src/lib/components/Icon/System/Eye';
-export * from '../src/lib/components/Icon/System/EyeNot';
-export * from '../src/lib/components/Icon/System/Mic';
-export * from '../src/lib/components/Icon/System/Search';
-export * from '../src/lib/components/Icon/System/Search';
+export * from './lib/components/Icon/System/Eye';
+export * from './lib/components/Icon/System/EyeNot';
+export * from './lib/components/Icon/System/Mic';
+export * from './lib/components/Icon/System/Search';
+export * from './lib/components/Icon/System/Search';
 // Icon Trilha
-export * from '../src/lib/components/Icon/Trilha/Aprender';
-export * from '../src/lib/components/Icon/Trilha/Defeciencia';
-export * from '../src/lib/components/Icon/Trilha/Curtir';
-export * from '../src/lib/components/Icon/Trilha/Empresario';
-export * from '../src/lib/components/Icon/Trilha/Idoso';
-export * from '../src/lib/components/Icon/Trilha/Lgbt';
-export * from '../src/lib/components/Icon/Trilha/Mulher';
-export * from '../src/lib/components/Icon/Trilha/Servidor';
-export * from '../src/lib/components/Icon/Trilha/Turista';
+export * from './lib/components/Icon/Trilha/Aprender';
+export * from './lib/components/Icon/Trilha/Defeciencia';
+export * from './lib/components/Icon/Trilha/Curtir';
+export * from './lib/components/Icon/Trilha/Empresario';
+export * from './lib/components/Icon/Trilha/Idoso';
+export * from './lib/components/Icon/Trilha/Lgbt';
+export * from './lib/components/Icon/Trilha/Mulher';
+export * from './lib/components/Icon/Trilha/Servidor';
+export * from './lib/components/Icon/Trilha/Turista';
 //Atomic
-export * from '../src/lib/components/Atomic/Avatar/Avatar';
-export * from '../src/lib/components/Atomic/Button/Button';
-export * from '../src/lib/components/Atomic/Checkbox/Checkbox';
-export * from '../src/lib/components/Atomic/Dropdown/Dropdown';
-export * from '../src/lib/components/Atomic/Input/Input';
-export * from '../src/lib/components/Atomic/Radio/Radio';
+export * from './lib/components/Atomic/Avatar/Avatar';
+export * from './lib/components/Atomic/Button/Button';
+export * from './lib/components/Atomic/Checkbox/Checkbox';
+export * from './lib/components/Atomic/Dropdown/Dropdown';
+export * from './lib/components/Atomic/Input/Input';
+export * from './lib/components/Atomic/Radio/Radio';
 // Molecules
-export * from '../src/lib/components/Molecules/Table/Table';
+export * from './lib/components/Molecules/Table/Table';
 //Template
-export * from '../src/lib/components/Templates/Header';
+export * from './lib/components/Templates/Header';
