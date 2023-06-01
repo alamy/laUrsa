86596ca5ef9bde1154b9d9bb676ecf83c22eac04
a.verissimo <alamy.neto@acpgroup.com.br>
2023-06-01 15:20:40 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 
+import { Header } from './lib/components/Templates/Header/Header';
 import './lib/styles/global.css';
 
 function App() {
@@ -7,7 +8,13 @@ function App() {
   return (
     
     <>
-
+       <Header
+                title="Prefeitura do Recife"
+                home={true}
+                persona={'Alamy Neto'}
+                key={'000000'}
+                path="/"
+            />
     </>
 
   )
--- src/lib/components/Templates/Header/Header.tsx
+++ src/lib/components/Templates/Header/Header.tsx
@@ -51,7 +51,7 @@ const Perfil = () => {
                     
                 </div>
 
-                <div className={clsx('home', {'block': home === true, 'none': home === false})} >
+                <div className={clsx('home', {'inline-block': home === true, 'none': home === false})} >
                    <div className='btnHome' onClick={Home} >
                     <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M4.5 13.5L18 3L31.5 13.5V30C31.5 30.7956 31.1839 31.5587 30.6213 32.1213C30.0587 32.6839 29.2956 33 28.5 33H7.5C6.70435 33 5.94129 32.6839 5.37868 32.1213C4.81607 31.5587 4.5 30.7956 4.5 30V13.5Z" stroke="#0F57BF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
