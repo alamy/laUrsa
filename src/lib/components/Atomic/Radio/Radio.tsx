2a5e93c9aa65b5bb767ca005ee54ea9f1e8f456b
Gustavo Jose Miranda de Souza <gustavo.miranda@demprel.local>
2023-07-19 10:47:28 -0300

    Corrigindo radio button

--- src/lib/components/Atomic/Radio/Radio.tsx
+++ src/lib/components/Atomic/Radio/Radio.tsx
@@ -1,50 +1,45 @@
-import { clsx } from 'clsx';
-import './Radio.scss'
+import { clsx } from "clsx";
+import "./Radio.scss";
 export interface RadioProps {
-    text: string;
-    disable?: boolean;
-    size?:'sm' | 'md' | 'lg'  ; 
+  text: string;
+  disable?: boolean;
+  size?: "sm" | "md" | "lg";
 }
 
-export function Radio({disable, text, size}: RadioProps ) {
-    let label;
-    let checked;
-    if(disable){
-        label = 'color-gray',
-        checked = 'checkmark-false'
-        
-    }else {
-        label = 'color'
-        checked = 'checkmark-true'
-    }
-    return (
+export function Radio({ disable, text, size }: RadioProps) {
+  let label;
+  let checked;
+  if (disable) {
+    (label = "color-gray"), (checked = "checkmark-false");
+  } else {
+    label = "color";
+    checked = "checkmark-true";
+  }
+  return (
     <>
-
-<label className={clsx(
-                    label,
-                    "container",
-                    {
-                        'container-font-lg': size === 'lg',
-                        'container-font-md': size === 'md',
-                        'container-font-sm': size === 'sm',
-                    },
-                    )
-                }>One
-  <input type="radio" name={text} id={text} disabled={disable} />
-  <span className={clsx(
-                "checkmark",
-                checked,
-                {
-                    'radio-grande': size === 'lg',
-                    'radio-medio': size === 'md',
-                    'radio-pequeno': size === 'sm',
-                },
-                )} >
-                </span>
-</label>
-
-        
-    </>  
-    );
- 
+      <label
+        className={clsx(label, "container", {
+          "container-font-lg": size === "lg",
+          "container-font-md": size === "md",
+          "container-font-sm": size === "sm",
+        })}
+      >
+        {text}
+        <input
+          type="radio"
+          name={text}
+          id={text}
+          disabled={disable}
+          className={clsx("radio")}
+        />
+        <span
+          className={clsx("checkmark", checked, {
+            "radio-grande": size === "lg",
+            "radio-medio": size === "md",
+            "radio-pequeno": size === "sm",
+          })}
+        ></span>
+      </label>
+    </>
+  );
 }
