e7f3c576d00846e80f60c53c80f1716f4af65fe8
a.verissimo <alamy.neto@acpgroup.com.br>
2024-01-08 13:19:41 -0300

    Versionamento

--- src/lib/components/Icon/System/Percent.tsx
+++ src/lib/components/Icon/System/Percent.tsx
@@ -39,10 +39,12 @@ export function Percent({type = 'Primary', size = 'md', value}: PercentProps ) {
     }   
 
     return (
-        <svg width={width} height={height} viewBox="0 0 24 24" values={value} fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M18 6L6 18" stroke={estilo} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M6 6L18 18" stroke={estilo} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M19 5L5 19" stroke={estilo} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M17.5 20C18.8807 20 20 18.8807 20 17.5C20 16.1193 18.8807 15 17.5 15C16.1193 15 15 16.1193 15 17.5C15 18.8807 16.1193 20 17.5 20Z" stroke={estilo} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M6.5 9C7.88071 9 9 7.88071 9 6.5C9 5.11929 7.88071 4 6.5 4C5.11929 4 4 5.11929 4 6.5C4 7.88071 5.11929 9 6.5 9Z" stroke={estilo} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
+        
               
         
     );
